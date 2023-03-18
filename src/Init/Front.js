a5a324d4e2053eb19e2310585c332b7b58c3fd04
Javier Hernandez Gonzalez <86983739+Arguish@users.noreply.github.com>
2023-03-18 19:22:58 +0000

    bootloDER

--- src/Init/Front.js
+++ src/Init/Front.js
@@ -1,1 +1,10 @@
 console.log("front ONLINE");
+
+let publisher = document.querySelectorAll("#publisher");
+
+console.log(publisher);
+
+publisher.forEach((element, i) => {
+  setTimeout(() => (element.className = "show"), 1000 * i);
+  setTimeout(() => (element.className = "hide"), 1000 * i + 3000);
+});

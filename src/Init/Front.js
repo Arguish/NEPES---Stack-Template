7fb78a4a650278b37394e4b7809e5a42684641be
Javier Hernandez Gonzalez <86983739+Arguish@users.noreply.github.com>
2023-03-19 19:30:42 +0000

    Añadida pantalla de carga

--- src/Init/Front.js
+++ src/Init/Front.js
@@ -4,7 +4,29 @@ let publisher = document.querySelectorAll("#publisher");
 
 console.log(publisher);
 
-publisher.forEach((element, i) => {
-  setTimeout(() => (element.className = "show"), 1000 * i);
-  setTimeout(() => (element.className = "hide"), 1000 * i + 3000);
-});
+function loopPublishers(array) {
+  let exit = 0;
+  array.forEach((element, i) => {
+    exit += i + 1;
+    showElem(element, i + 1, i + 1);
+    if (i === array.length - 1) {
+      setTimeout(
+        () => (window.location.href = "../Main Page/index.html"),
+        1000 * exit
+      );
+    }
+  });
+}
+
+function showElem(elem, segs, hide) {
+  setTimeout(() => {
+    elem.className = "show";
+    hideElem(elem, hide);
+  }, 1000 * segs);
+}
+
+function hideElem(elem, segs) {
+  setTimeout(() => (elem.className = "hide"), 1000 * segs);
+}
+
+loopPublishers(publisher);
